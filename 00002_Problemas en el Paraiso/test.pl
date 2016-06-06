6bbefd343c3854ad6ea27717416185158dd30cd7
crespi.gustavo@gmail.com <crespi.gustavo@gmail.com>
2016-06-06 17:13:15 -0400

    Mumuki Export on 2016-06-06 17:13:15 -0400

--- 00002_Problemas en el Paraiso/test.pl
+++ 00002_Problemas en el Paraiso/test.pl
@@ -4,7 +4,7 @@ test(juan_prefiere_a_marta_antes_que_a_su_pareja_ana):-
 test(ana_prefiere_a_juan_antes_que_a_su_pareja_pedro):-
 	loPrefiereAntesQueASuPareja(ana, juan, [pareja(pedro, ana), pareja(juan, marta)]).
 	
-test(ana_prefiere_a_juan_o_luis_antes_que_a_su_pareja_pedro, set(Persona == [juan, pedro])):-
+test(ana_prefiere_a_juan_o_luis_antes_que_a_su_pareja_pedro, set(Persona == [juan, luis])):-
 	loPrefiereAntesQueASuPareja(ana, Persona, [pareja(pedro, ana), pareja(juan, marta), pareja(luis, nora)]).
 
 test(ana_prefiere_a_juan_o_luis_antes_que_a_su_pareja_pedro, set(Persona == [ana])):-
