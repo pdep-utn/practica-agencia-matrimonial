2eff7af9f76af2e264cdee1c75cecf92ad412e7e
crespi.gustavo@gmail.com <crespi.gustavo@gmail.com>
2016-06-06 17:16:38 -0400

    Mumuki Export on 2016-06-06 17:16:38 -0400

---
+++ 00003_Adulterio/extra.pl
@@ -0,0 +1,15 @@
+enPareja(PersonaA, PersonaB, pareja(PersonaA, PersonaB)).
+enPareja(PersonaB, PersonaA, pareja(PersonaA, PersonaB)).
+
+prefiereAAntesQue(Persona, Preferido, Rechazado):-
+    preferencias(Persona, Preferidos),
+    nth0(PosicionRechazo, Preferidos, Rechazado),
+    nth0(PosicionPreferido, Preferidos, Preferido),
+    PosicionRechazo > PosicionPreferido.
+    
+loPrefiereAntesQueASuPareja(PersonaA, PersonaC, Contexto):-
+    member(Pareja1, Contexto),
+    member(Pareja2, Contexto),
+    enPareja(PersonaA, PersonaB, Pareja1),
+    enPareja(PersonaC, _, Pareja2),
+    prefiereAAntesQue(PersonaA, PersonaC, PersonaB).
